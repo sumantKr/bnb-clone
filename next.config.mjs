1ad222df6e26b640ea8b2e5ba9581369614488fe
SumantKumar <sumant.kumar@antino.io>
2024-03-12 23:33:15 +0530

    next config original

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,4 @@
-import withAntdLess from "next-plugin-antd-less"
 /** @type {import('next').NextConfig} */
-const nextConfig = withAntdLess({
-    // optional: you can modify antd less variables directly here
-    modifyVars: { '@primary-color': '#333333' },
-    // Or better still you can specify a path to a file 
-    // lessVarsFilePath: '/src/styles/variables.less',
-    // optional
-    lessVarsFilePathAppendToEndOfContent: false,
-    // optional https://github.com/webpack-contrib/css-loader#object
-    cssLoaderOptions: {},
-
-    // Other Config Here...
-
-    webpack(config) {
-        return config;
-    },
-
-    // ONLY for Next.js 10, if you use Next.js 11, delete this block
-    // future: {
-    //     webpack5: true,
-    // },
-});;
+const nextConfig = {};
 
 export default nextConfig;
